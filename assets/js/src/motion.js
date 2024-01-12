9056ac6cf5d56faa449ba9a9fa65131964f9f753
Kyujin <kjsl4tw@naver.com>
2024-01-12 11:30:04 +0900

    Update motion.js

--- assets/js/src/motion.js
+++ assets/js/src/motion.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
   sidebarToggleLines.push(sidebarToggleLine2nd);
   sidebarToggleLines.push(sidebarToggleLine3rd);
 
-  var SIDEBAR_WIDTH = '320px';
+  var SIDEBAR_WIDTH = '500px';
   var SIDEBAR_DISPLAY_DURATION = 200;
 
   var sidebarToggleMotion = {
